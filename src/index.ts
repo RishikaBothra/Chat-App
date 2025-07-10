ef18502c359ede1b1b57b18fc40e66ccaa107181
RishikaBothra <rshkbothra@gmail.com>
2025-07-11 00:33:40 +0530

    feat: implement room-based messaging with user-room tracking

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,43 @@ import { WebSocketServer,WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080, host: "0.0.0.0" });
 
-
+interface User{
+    socket: WebSocket;
+    room:string;
+}
 let userCount = 0;
-let allSockets: WebSocket[] = [];
+let allSockets: User[] = [];
 
 wss.on("connection",(socket) =>{
-    allSockets.push(socket);
-
-    userCount++;
-    console.log("user count:", userCount);
 
     socket.on("message",(message) =>{
-        console.log("msg:",message.toString()," from user:", userCount);
-        for (let i = 0; i < allSockets.length; i++) {
-            allSockets[i].send(message.toString() + " from user: " + userCount);
+
+        const parsedMessage = JSON.parse(message as unknown as string);
+        if(parsedMessage.type == "join") {
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
+        }
+        if(parsedMessage.type == "message"){
+            // const currentUserRoom = allSockets.find((x) =>x.socket == socket).room;
+            let currentUserRoom = null;
+            for(let i = 0; i < allSockets.length; i++) {
+                if(allSockets[i].socket === socket) {
+                    currentUserRoom = allSockets[i].room;
+                    break;
+                }
+            }
+            for(let i = 0; i < allSockets.length; i++) {
+                if(allSockets[i].room === currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     })
-    socket.on("disconnect",() =>{
-        allSockets = allSockets.filter(x=>x!=socket);
-        userCount--;
-        console.log("user count:", userCount);
-    })
+//     socket.on("disconnect",() =>{
+//         allSockets = allSockets.filter(x=>x!=socket);
+//         userCount--;
+//         console.log("user count:", userCount);
+//     })
 })
